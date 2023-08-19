5c037cca0a7720682565c7ac4be95d53048924e2
subhajitdas1999 <sd651846@gmail.com>
2023-08-20 01:37:16 +0530

    fixed typo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import Header from './Header.tsx';
+import Header from "./Header.tsx";
 
 function App() {
   return (
-    <div className=' bg-dark h-screen w-screen px-10 py-5'>
+    <div className=" bg-dark h-screen w-screen px-10 py-5">
       <Header />
-     
       {/* Your main content */}
     </div>
   );
