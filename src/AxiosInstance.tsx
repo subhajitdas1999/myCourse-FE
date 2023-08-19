0d53cbb3b25285611e268891d5e41f81031c6f29
subhajitdas1999 <sd651846@gmail.com>
2023-08-20 01:38:29 +0530

    Remove withCredentials, add this property whenever needed in a request

--- src/AxiosInstance.tsx
+++ src/AxiosInstance.tsx
@@ -3,7 +3,6 @@ import axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
-  withCredentials: true,
   // You can add more default config options here
 });
 
