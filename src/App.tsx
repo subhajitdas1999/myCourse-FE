a9c4efc419116f0cea4a516552fab52ebcadd759
subhajitdas1999 <sd651846@gmail.com>
2023-08-25 00:27:56 +0530

    added intital Courses componect

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Courses from "./Courses.tsx";
 import Header from "./Header.tsx";
 
 function App() {
   return (
     <div className=" bg-dark h-screen w-screen px-10 py-5">
       <Header />
+      <Courses />
       {/* Your main content */}
     </div>
   );
